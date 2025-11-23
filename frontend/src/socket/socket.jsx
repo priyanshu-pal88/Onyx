3921d13232e78ded5df05a37f4d810bf2961b3eb
priyanshu-pal88 <aachipal88@gmail.com>
2025-11-23 21:44:14 +0530

    final commit

--- frontend/src/socket/socket.jsx
+++ frontend/src/socket/socket.jsx
@@ -1,7 +1,7 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io(import.meta.env.VITE_BACKEND_URL, {
   autoConnect: false,
   transports: ["websocket"],
   withCredentials: true,
